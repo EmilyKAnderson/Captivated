7dcb168d84a32b9089f02cec1f28cc5de52b9840
Emily K. Anderson <eanderson@wearearchitect.com>
2017-01-24 19:20:38 -0800

    added to photos for automotive and updated gulp task

--- _gulp/tasks/javascript.js
+++ _gulp/tasks/javascript.js
@@ -4,34 +4,34 @@
 // ---
 
 // plugins
-// var gulp = require('gulp'),
-//     uglyfly = require('gulp-uglyfly');
-//
-// var config = require('../config').compressjs;
-//
-// //task
-// gulp.task('compressjs', function() {
-//     gulp.src(config.src)
-//         .pipe(uglyfly())
-//         .pipe(gulp.dest(config.dest));
-// });
+var gulp = require('gulp'),
+    uglyfly = require('gulp-uglyfly');
+
+var config = require('../config').js;
+
+//task
+gulp.task('js', function() {
+    gulp.src(config.src)
+        .pipe(uglyfly())
+        .pipe(gulp.dest(config.jekyllJsDest));
+});
 
 
 // plugins
-var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    uglyfly = require('gulp-uglyfly');
-    concat = require('gulp-concat');
-
-// configfile
-var config = require('../config').js;
-
-// tasks
-gulp.task('js', function () {
-    return gulp.src(config.src)
-        .pipe(plumber())
-        .pipe(uglyfly())
-        .pipe(concat(config.concatFilename))
-        .pipe(gulp.dest(config.jekyllJsDest))
-        .pipe(gulp.dest(config.jsDest));
-});
+// var gulp = require('gulp'),
+//     plumber = require('gulp-plumber'),
+//     uglyfly = require('gulp-uglyfly');
+//     concat = require('gulp-concat');
+//
+// // configfile
+// var config = require('../config').js;
+//
+// // tasks
+// gulp.task('js', function () {
+//     return gulp.src(config.src)
+//         .pipe(plumber())
+//         .pipe(uglyfly())
+//         .pipe(concat(config.concatFilename))
+//         .pipe(gulp.dest(config.jekyllJsDest))
+//         .pipe(gulp.dest(config.jsDest));
+// });
